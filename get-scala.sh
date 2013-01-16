8a92a8daeb37f873a82a16cad7e683f36e453015
Eemil Lagerspetz <eemil.lagerspetz@gmail.com>
2013-01-16 22:09:54 +0200

    Do not delete spark automatically. Set save names.

--- get-scala.sh
+++ get-scala.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget http://www.scala-lang.org/downloads/distrib/files/scala-2.9.2.tgz
+wget http://www.scala-lang.org/downloads/distrib/files/scala-2.9.2.tgz -O scala-2.9.2.tgz
 tar xzf scala-2.9.2.tgz
 
--- get-spark.sh
+++ get-spark.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-wget http://spark-project.org/download-spark-0.6.1-sources-tgz
+wget http://spark-project.org/download-spark-0.6.1-sources-tgz -O spark-0.6.1-sources.tgz
 tar xzf spark-0.6.1-sources.tgz
 cd spark-0.6.1
 sbt/sbt compile assembly
-cp core/target/spark-*jars
+cp core/target/spark-* ../jars
 cd ..
-rm -rf spark-0.6.1
 
